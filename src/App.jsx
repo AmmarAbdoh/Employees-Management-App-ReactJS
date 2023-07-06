fd7b58404ba4fb2cca9da1cc6898c864cc54f18e
Ammar Abdo <90091361+AmmarAbdoh@users.noreply.github.com>
2023-07-06 17:28:31 +0300

    removed files and added a form to add new cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,74 +1,30 @@
 import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.css";
-import { Card, Container, Row, Col, Form, Button } from "react-bootstrap";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
 import "./App.css";
-import FormComponent from "./form";
-
-function IdentityCard({ info }) {
-  const { name, job, email, phone, img } = info;
-
-  const renderImage = () => {
-    if (img) {
-      return <Card.Img src={img} alt="EMPLOYEE" className="card-image" />;
-    }
-    return null;
-  };
-
-  return (
-    <div>
-      {phone ? (
-        <Card className="bg-primary text-white text-center fixed-card">
-          {renderImage()}
-          <Card.Body>
-            <Card.Title>
-              <p className="fs-3 font-size-xs font-size-md">{name}</p>
-            </Card.Title>
-            <Card.Text>
-              <p className="fs-3 font-size-xs font-size-md">{job}</p>
-              <p className="fs-5 font-size-xs font-size-md">Phone : {phone}</p>
-              <p className="fs-5 font-size-xs font-size-md">Email : {email}</p>
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      ) : (
-        <Card className="bg-danger text-white text-center fixed-card">
-          {renderImage()}
-          <Card.Body>
-            <Card.Title>
-              <h2>{name}</h2>
-            </Card.Title>
-            <Card.Text>
-              <p className="fs-3">{job}</p>
-              <p className="fs-5">Email : {email}</p>
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      )}
-    </div>
-  );
-}
+import IdentityCard from "./IdentityCard.jsx";
 
 function App() {
   const [employees, setEmployee] = useState([]);
   const [formData, setFormData] = useState({
     name: "",
-    job: "",
+    position: "",
     email: "",
     phone: "",
-    image: null,
+    img: null,
   });
 
   const handleChange = (e) => {
-    if (e.target.name === "image") {
+    if (e.target.name === "img") {
       const file = e.target.files[0];
       if (file) {
         const reader = new FileReader();
         reader.onloadend = () => {
-          setFormData({ ...formData, image: reader.result });
+          setFormData({ ...formData, img: reader.result });
         };
         reader.readAsDataURL(file);
       } else {
-        setFormData({ ...formData, image: null });
+        setFormData({ ...formData, img: null });
       }
     } else {
       setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -80,22 +36,27 @@ function App() {
     const newEmployee = {
       id: generateId(),
       ...formData,
-      img: formData.image,
+      img: formData.img,
     };
     setEmployee([...employees, newEmployee]);
     setFormData({
       name: "",
-      job: "",
+      position: "",
       email: "",
       phone: "",
-      image: null,
+      img: null,
     });
+    document.getElementById("formImg").value = "";
   };
 
   const generateId = () => {
-    // Logic to generate a unique ID (e.g., using a library like uuid)
-    // For simplicity, we'll use a random number for the ID in this example
-    return Math.floor(Math.random() * 1000);
+    const maxId = employees.reduce((max, employee) => {
+      return employee.id > max ? employee.id : max;
+    }, 0);
+
+    const newId = maxId + 1;
+
+    return newId;
   };
 
   const columnSizes = {
@@ -107,69 +68,89 @@ function App() {
   };
   return (
     <>
-      <Container className="mx-auto text-center mb-5">
-        <h1 className="mt-4 mb-3">Employees Form</h1>
-        <Row>
-          <Col>
-            <Form onSubmit={handleSubmit}>
-              <Form.Group controlId="formName" className="mb-3">
-                <Form.Label>Name:</Form.Label>
-                <Form.Control
-                  required
-                  type="text"
-                  name="name"
-                  value={formData.name}
-                  onChange={handleChange}
-                />
-              </Form.Group>
-              <Form.Group controlId="formJob" className="mb-3">
-                <Form.Label>Job:</Form.Label>
-                <Form.Control
-                  required
-                  type="text"
-                  name="job"
-                  value={formData.job}
-                  onChange={handleChange}
-                />
-              </Form.Group>
-              <Form.Group controlId="formEmail" className="mb-3">
-                <Form.Label>Email:</Form.Label>
-                <Form.Control
-                  required
-                  type="email"
-                  name="email"
-                  value={formData.email}
-                  onChange={handleChange}
-                />
-              </Form.Group>
-              <Form.Group controlId="formPhone" className="mb-3">
-                <Form.Label>Phone:</Form.Label>
-                <Form.Control
-                  type="tel"
-                  name="phone"
-                  value={formData.phone}
-                  onChange={handleChange}
-                />
-              </Form.Group>
-              <Form.Group controlId="formImage" className="mb-3">
-                <Form.Label>Image:</Form.Label>
-                <Form.Control
-                  required
-                  type="file"
-                  name="image"
-                  accept="image/*"
-                  onChange={handleChange}
-                />
-              </Form.Group>
-              <Button variant="primary" type="submit">
-                Add Employee
-              </Button>
-            </Form>
-          </Col>
-        </Row>
+      <Container
+        className="form-background d-flex align-items-center vh-100"
+        fluid
+      >
+        <Container>
+          <h1 className="text-center mb-5 text-white fw-bold">
+            Employees Form
+          </h1>
+          <Row className="justify-content-center">
+            <Col xs={12} sm={8} md={6} lg={4}>
+              <Form onSubmit={handleSubmit}>
+                <Form.Group controlId="formName" className="mb-3">
+                  <Form.Label>Name:</Form.Label>
+                  <Form.Control
+                    required
+                    type="text"
+                    name="name"
+                    value={formData.name}
+                    onChange={handleChange}
+                    className="text-box"
+                  />
+                </Form.Group>
+                <Form.Group controlId="formPosition" className="mb-3">
+                  <Form.Label>Position:</Form.Label>
+                  <Form.Control
+                    required
+                    type="text"
+                    name="position"
+                    value={formData.position}
+                    onChange={handleChange}
+                    className="text-box"
+                  />
+                </Form.Group>
+                <Form.Group controlId="formEmail" className="mb-3">
+                  <Form.Label>Email:</Form.Label>
+                  <Form.Control
+                    required
+                    type="email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                    className="text-box"
+                  />
+                </Form.Group>
+                <Form.Group controlId="formPhone" className="mb-3">
+                  <Form.Label>Phone:</Form.Label>
+                  <Form.Control
+                    type="tel"
+                    name="phone"
+                    value={formData.phone}
+                    onChange={handleChange}
+                    className="text-box"
+                  />
+                </Form.Group>
+                <Form.Group controlId="formImg" className="mb-3">
+                  <Form.Label>Image:</Form.Label>
+                  <Form.Control
+                    required
+                    type="file"
+                    name="img"
+                    accept="img/*"
+                    onChange={handleChange}
+                    className="text-box"
+                  />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                  Add Employee
+                </Button>
+              </Form>
+            </Col>
+          </Row>
+        </Container>
       </Container>
 
       <Container className="mx-auto">
+        {employees.length > 0 && (
+          <h1
+            className="text-center fw-bold mt-4 mb-3"
+            style={{ color: "darkblue" }}
+          >
+            Employees Cards
+          </h1>
+        )}
         <Row className="justify-content-center">
           {employees.map((employee) => (
             <Col {...columnSizes} key={employee.id}>
---
+++ src/IdentityCard.jsx
@@ -0,0 +1,48 @@
+import { Card } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+
+function IdentityCard({ info }) {
+  const { name, position, email, phone, img } = info;
+
+  const renderImage = () => {
+    if (img) {
+      return <Card.Img src={img} alt="EMPLOYEE" className="card-image" />;
+    }
+    return null;
+  };
+
+  return (
+    <div>
+      {phone ? (
+        <Card className="bg-primary text-white text-center fixed-card">
+          {renderImage()}
+          <Card.Body>
+            <Card.Title>
+              <p className="fs-3 font-size-xs font-size-md">{name}</p>
+            </Card.Title>
+            <Card.Text>
+              <p className="fs-3 font-size-xs font-size-md">{position}</p>
+              <p className="fs-5 font-size-xs font-size-md">Phone : {phone}</p>
+              <p className="fs-5 font-size-xs font-size-md">Email : {email}</p>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      ) : (
+        <Card className="bg-danger text-white text-center fixed-card">
+          {renderImage()}
+          <Card.Body>
+            <Card.Title>
+              <h2>{name}</h2>
+            </Card.Title>
+            <Card.Text>
+              <p className="fs-3">{position}</p>
+              <p className="fs-5">Email : {email}</p>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      )}
+    </div>
+  );
+}
+
+export default IdentityCard;
