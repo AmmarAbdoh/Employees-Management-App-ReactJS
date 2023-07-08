3f7cb9df689ab11b06e07c19f17fc2de468c6a4a
Ammar Abdo <90091361+AmmarAbdoh@users.noreply.github.com>
2023-07-08 14:41:02 +0300

    fixed card style when not having a phone

--- src/IdentityCard.jsx
+++ src/IdentityCard.jsx
@@ -14,32 +14,40 @@ function IdentityCard(props) {
     const newEmployees = props.employees.filter((card) => card.id !== id);
     props.setEmployees(newEmployees);
   };
-        
+
   const cardBgClassName = phone ? "card-bg" : "card2-bg";
 
   return (
-        <div>
-            <Card className={`text-center fixed-card ${cardBgClassName} card-text`}>
-            { renderImage() }
-            <Card.Body>
-                <Card.Title>
-                    <p className="fs-4 font-size-xs font-size-md fw-bold">{ name }</p>
-                </Card.Title>
-                <Card.Text>
-                    <p className="fs-5 font-size-xs font-size-md fw-bold">{ position }</p>
-                    { phone && <p className="fs-6 font-size-xs font-size-md">Phone: { phone }</p> }
-                    <p className="fs-6 font-size-xs font-size-md">Email: { email }</p>
-                </Card.Text>
-                <Button
-                    style={ phone ? { backgroundColor: "#4191fc" } : { backgroundColor: "#b5d3e1" } }
-                    onClick={ deleteCard }
-                    className="fw-bold">
-                    Remove Card
-                </Button>
-            </Card.Body>
-        </Card>
-    </div >
-    );
+    <div>
+      <Card className={`text-center fixed-card ${cardBgClassName} card-text`}>
+        {renderImage()}
+        <Card.Body>
+          <Card.Title>
+            <p className="fs-4 font-size-xs font-size-md fw-bold">{name}</p>
+          </Card.Title>
+          <Card.Text>
+            <p className="fs-5 font-size-xs font-size-md fw-bold">{position}</p>
+            {phone && (
+              <p className="fs-6 font-size-xs font-size-md">Phone: {phone}</p>
+            )}
+            <p className="fs-6 font-size-xs font-size-md">Email: {email}</p>
+            {!phone && <p className="fs-6 font-size-xs font-size-md">&nbsp;</p>}
+          </Card.Text>
+          <Button
+            style={
+              phone
+                ? { backgroundColor: "#4191fc" }
+                : { backgroundColor: "#b5d3e1" }
+            }
+            onClick={deleteCard}
+            className="fw-bold"
+          >
+            Remove Card
+          </Button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 }
 
 export default IdentityCard;
